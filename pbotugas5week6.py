00c4f2aaa654ce88db12687e5ebf80821c2051cf
halhalobandung <anta3406@gmail.com>
2025-11-04 21:08:00 +0700

    update

--- pbotugas5week6.py
+++ pbotugas5week6.py
@@ -5,6 +5,35 @@ class Nasabah:
         self.__saldo_awal = saldo_awal
         self._poin_reward = poin_reward
 
-    def menabung(self):
+    def tampilkan_info(self):
+        print("==Informasi Nasabah==")
+        print(f"Nama :{self.nama}")
+        print(f"Nomor Rekening :{self.no_rek}")
+        print(f"Saldo :Rp{self.__saldo_awal}")
+        print(f"Poin Reward :{self._poin_reward}")
+        print("="*21)
+        return self
+    
+    def cek_saldo(self):
+        print(f"Saldo anda saat ini sebesar: Rp{self.__saldo_awal}")
 
-    def belanja(self):
+    def menabung(self, tabungan):
+        if tabungan <= 0:
+            print("==Anda Belum Ada Saldo==")
+            return self
+        
+        cashback = tabungan * 0.01
+        self.__saldo_awal += (tabungan + cashback)
+
+        waktu_transaksi = datetime.datetime.now()
+        self._histori_transaksi("Menabung", tabungan, waktu_transaksi)
+        self._histori_transaksi("cashback", cashback, waktu_transaksi)
+
+        print(f"Menabung Rp{tabungan:,.} berhasil. Anda mendapatkan Cashback sebesar Rp{cashback:,.}.")
+        self.cek_saldo()
+
+    def 
+
+    def belanja(self, belanjaan):
+        if belanjaan <= 0:
+            print("==Anda Belum Belanja==")
