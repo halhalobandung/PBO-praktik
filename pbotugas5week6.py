0a96d67ae9b40eb4eb86d8a3b7d5cee1bc5e14d5
halhalobandung <anta3406@gmail.com>
2025-11-05 10:37:25 +0700

    updatehariini

--- pbotugas5week6.py
+++ pbotugas5week6.py
@@ -1,24 +1,14 @@
-import datetime
-
 class Nasabah:
-    def __init__(self, nama, no_rek, saldo_awal, poin_reward):
-        self.nama = nama
+    def __init__(self, nama, no_rek, saldo_awal):
+        self._nama = nama
         self.no_rek = no_rek
         self.__saldo_awal = saldo_awal
-        self._poin_reward = poin_reward
-        self._riwayat_transaksi = []
+        self.__poin_reward = 0
+        self.__riwayat_transaksi = []
+        self.__log(f"Open Account saldo awal: {saldo_awal}")
 
-    def tampilkan_info(self):
-        print("==Informasi Nasabah==")
-        print(f"Nama :{self.nama}")
-        print(f"Nomor Rekening :{self.no_rek}")
-        print(f"Saldo :Rp{self.__saldo_awal}")
-        print(f"Poin Reward :{self._poin_reward}")
-        print("="*21)
-        return self
-    
-    def cek_saldo(self):
-        print(f"Saldo anda saat ini sebesar: Rp{self.__saldo_awal}")
+    def __log(self, text):
+        self.__riwayat_transaksi(text)
 
     def menabung(self, tabungan):
         if tabungan <= 0:
@@ -26,23 +16,10 @@ class Nasabah:
             return self
         
         cashback = tabungan * 0.01
-        self.__saldo_awal += (tabungan + cashback)
-
-        waktu_transaksi = datetime.datetime.now()
-        self._histori_transaksi("Menabung", tabungan, waktu_transaksi)
-        self._histori_transaksi("cashback", cashback, waktu_transaksi)
-
+        self.__saldo_awal += tabungan + cashback
         print(f"Menabung Rp{tabungan:,.} berhasil. Anda mendapatkan Cashback sebesar Rp{cashback:,.}.")
+        self.__log(f"Menabung Rp{tabungan:,} | cashback RP(cashback:,.)")
         self.cek_saldo()
-
-    def histori_transaksi(self, deskripsi, jumlah, waktu, is_poin=False):
-        self._riwayat_transaksi.append({
-            "Waktu": waktu,
-            "Deskripsi": deskripsi,
-            "Jumlah": jumlah,
-            "is_poin": is_poin
-        })
-        return self
 
     def belanja(self, belanjaan):
         if belanjaan <= 0:
@@ -51,21 +28,23 @@ class Nasabah:
         
         if self.__saldo_awal >= belanjaan:
             self.__saldo_awal -= belanjaan
-            poinreward = int(belanjaan // 10000)
-            self._poin_reward += poinreward
+            poinreward = belanjaan // 10000
+            self.__poin_reward += poinreward
+            self.__log(f"Belanja Rp")
 
-            self._histori_transaksi("Belanja", -belanjaan, datetime.datetime.now())
-
-            print(f"Belanja sebesar Rp{belanjaan:,.} berhasil.")
-            if poinreward > 0:
-                print(f"Selamat! Anda mendapatkan {poinreward} Poin Reward.")
-            self.cek_saldo()
+            # print(f"Belanja sebesar Rp{belanjaan:,.} berhasil.")
+            # if poinreward > 0:
+            #     print(f"Selamat! Anda mendapatkan {poinreward} Poin Reward.")
+            # self.cek_saldo()
         else:
             print("Saldo Anda Tidak Mencukupi.")
             self.cek_saldo()
 
+    def cek_saldo(self):
+        print(f"Saldo anda saat ini sebesar: Rp{self.__saldo_awal}")
+
     def poin(self):
-        print(f"Poin Reward Anda: {self._poin_reward} poin")
+        print(f"Poin Reward Anda: {self.__poin_reward} poin")
 
     def poin_redeem(self, pilihan):
         list_hadiah = {
@@ -83,4 +62,13 @@ class Nasabah:
             self._poin_reward -= poin_dibutuhkan
             print(f"Anda berhasil menukarkan {poin_dibutuhkan} poin denga {hadiah['nama']}!")
         else:
-            print("Poin Anda Tidak cukup untuk menukarkan hadiah ini.")        
+            print("Poin Anda Tidak cukup untuk menukarkan hadiah ini.")
+
+    def tampilkan_info(self):
+        print("==Informasi Nasabah==")
+        print(f"Nama :{self.nama}")
+        print(f"Nomor Rekening :{self.no_rek}")
+        print(f"Saldo :Rp{self.__saldo_awal}")
+        print(f"Poin Reward :{self._poin_reward}")
+        print("="*21)
+        return self        
