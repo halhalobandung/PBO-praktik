491dd864c6035d46130d88bbabef194a3d0249b2
halhalobandung <anta3406@gmail.com>
2025-11-05 14:38:50 +0700

    update Fix Tugas PBO

--- pbotugas5week6.py
+++ pbotugas5week6.py
@@ -1,14 +1,35 @@
+from datetime import datetime
+
 class Nasabah:
+
+    no_rek = ""
+
     def __init__(self, nama, no_rek, saldo_awal):
         self._nama = nama
         self.no_rek = no_rek
         self.__saldo_awal = saldo_awal
         self.__poin_reward = 0
         self.__riwayat_transaksi = []
-        self.__log(f"Open Account saldo awal: {saldo_awal}")
 
-    def __log(self, text):
-        self.__riwayat_transaksi(text)
+        self.__log(f"Buka Rekening | saldo awal Rp {saldo_awal}")
+
+    def get_saldo(self):
+        #print(f"Saldo anda saat ini sebesar: Rp{self.__saldo_awal}")
+        return self.__saldo_awal
+    
+    def set_saldo(self, value):
+        self.__saldo_awal = value
+
+    def get_poin(self):
+        #print(f"Poin Reward Anda: {self.__poin_reward} poin")
+        return self.__poin_reward
+    
+    def set_poin(self, value):
+        self.__poin_reward = value
+
+    def __log(self, teks):
+        waktu = datetime.now().strftime("%d-%m-%Y %H:%M")
+        self.__riwayat_transaksi.append(f"[{waktu}] {teks}")
 
     def menabung(self, tabungan):
         if tabungan <= 0:
@@ -16,21 +37,24 @@ class Nasabah:
             return self
         
         cashback = tabungan * 0.01
-        self.__saldo_awal += tabungan + cashback
-        print(f"Menabung Rp{tabungan:,.} berhasil. Anda mendapatkan Cashback sebesar Rp{cashback:,.}.")
-        self.__log(f"Menabung Rp{tabungan:,} | cashback RP(cashback:,.)")
-        self.cek_saldo()
+        saldo_baru = self.get_saldo() + tabungan + cashback
+        self.set_saldo(saldo_baru)
+        print(f"Menabung Rp{tabungan:,} berhasil. Anda mendapatkan Cashback sebesar Rp{cashback:,}.")
+        self.__log(f"Menabung Rp{tabungan:,} | cashback RP{cashback:,}")
+        #self.cek_saldo()
 
     def belanja(self, belanjaan):
         if belanjaan <= 0:
             print("==Anda Belum Belanja==")
             return self
         
-        if self.__saldo_awal >= belanjaan:
-            self.__saldo_awal -= belanjaan
+        if self.get_saldo() >= belanjaan:
+            saldo_baru = self.get_saldo() - belanjaan
+            self.set_saldo(saldo_baru)
+
             poinreward = belanjaan // 10000
             self.__poin_reward += poinreward
-            self.__log(f"Belanja Rp")
+            self.__log(f"Belanja Rp{belanjaan:,} | Mendapatkan {poinreward} poin")
 
             # print(f"Belanja sebesar Rp{belanjaan:,.} berhasil.")
             # if poinreward > 0:
@@ -40,35 +64,60 @@ class Nasabah:
             print("Saldo Anda Tidak Mencukupi.")
             self.cek_saldo()
 
-    def cek_saldo(self):
-        print(f"Saldo anda saat ini sebesar: Rp{self.__saldo_awal}")
-
-    def poin(self):
-        print(f"Poin Reward Anda: {self.__poin_reward} poin")
-
-    def poin_redeem(self, pilihan):
+    def poin_redeem(self, jumlah_poin):
         list_hadiah = {
-                1: {"nama": "Voucher Belanja Rp50.000", "poin": 100},
-                2: {"nama": "Saldo Rp100.000", "poin": 200},
-                3: {"nama": "Smartwatch BCN Edition", "poin": 500}
+                100: "Voucher Belanja Rp50.000",
+                200: "Saldo Rp100.000",
+                500: "Smartwatch BCN Edition"
         }
 
-        if pilihan not in list_hadiah:
-            print("Pilihan Anda tidak valid")
+        if jumlah_poin not in list_hadiah:
+            print("Pilihan hadiah tidak tersedia!")
+            return
+        
+        if self.get_poin() < jumlah_poin:
+            print("Poin anda tidak mencukupi untuk redeem!")
+            return
+        
+        self.set_poin(self.get_poin() - jumlah_poin)
 
-        hadiah = list_hadiah[pilihan]
-        poin_dibutuhkan = hadiah["poin"]
-        if self._poin_reward >= poin_dibutuhkan:
-            self._poin_reward -= poin_dibutuhkan
-            print(f"Anda berhasil menukarkan {poin_dibutuhkan} poin denga {hadiah['nama']}!")
-        else:
-            print("Poin Anda Tidak cukup untuk menukarkan hadiah ini.")
+        if jumlah_poin == 200:
+            self.set_saldo(self.get_saldo() + 100000)
+
+        self.__log(f"REDEEM {list_hadiah[jumlah_poin]} | Potong poin {jumlah_poin}")
 
     def tampilkan_info(self):
-        print("==Informasi Nasabah==")
-        print(f"Nama :{self.nama}")
-        print(f"Nomor Rekening :{self.no_rek}")
-        print(f"Saldo :Rp{self.__saldo_awal}")
-        print(f"Poin Reward :{self._poin_reward}")
-        print("="*21)
-        return self        
+        print("="*50)
+        print("Informasi Nasabah")
+        print("="*50)
+        print(f"Nama            :{self._nama}")
+        print(f"Nomor Rekening  :{self.no_rek}")
+        print(f"Saldo           :Rp {self.get_saldo():,}")
+        print(f"Poin Reward     :{self.get_poin()}")
+        print("="*50)
+
+    def generate_riwayat_transaksi(self):
+        print("\n== DETAIL TRANSAKSI BANK CERDAS NUSANTARA ==")
+        print(f"Nasabah : {self._nama}")
+        print("-"*50)
+        for i, r in enumerate(self.__riwayat_transaksi, start=1):
+            print(f"{i}. {r}")
+        print("="*50)
+        print(f"Saldo Akhir : Rp {self.get_saldo():,}")
+        print(f"Poin Akhir : {self.get_poin()}")
+        print("="*50)
+
+nasabah1 = Nasabah("Anta Rizqi Maulana", "1234567890", 0)
+
+nasabah1.menabung(10_000_000)
+
+nasabah1.belanja(2_500_000)
+nasabah1.belanja(3_200_000)
+nasabah1.belanja(1_300_000)
+
+nasabah1.menabung(3_000_000)
+
+nasabah1.poin_redeem(200)
+
+nasabah1.tampilkan_info()
+nasabah1.generate_riwayat_transaksi()
